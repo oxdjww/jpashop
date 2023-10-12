2553294931805f18ab816e6567b921575960999a
oxdjww <oxdjww@gmail.com>
2023-10-12 20:00:28 +0900

    feat : add item repository

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            // id값이 없으면, 새로 생성한 item이라는 의미이다. (JPA 특징)
+            em.persist(item);
+        } else {
+            // id값이 있으면 update 같은 기능을 함
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
