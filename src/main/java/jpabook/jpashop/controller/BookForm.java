a91afac82b4e92104f8176fb855d7d30de76641f
oxdjww <oxdjww@gmail.com>
2023-10-18 21:41:23 +0900

    add : add udpateitemcontroller

--- src/main/java/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -10,8 +10,8 @@ public class BookForm {
 
     private String name;
     private int price;
-    private int stockQuantity;
 
+    private int stockQuantity;
     private String author;
     private String isbn;
 }
